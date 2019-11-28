7ca25c56fe78fa2e457b7621e744f524c780dda7
georgiev84 <cska30>
2019-11-29 00:11:15 +0200

    Initial commit

---
+++ src/main/java/com/petar/workspring/WorkspringApplication.java
@@ -0,0 +1,13 @@
+package com.petar.workspring;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WorkspringApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(WorkspringApplication.class, args);
+    }
+
+}
